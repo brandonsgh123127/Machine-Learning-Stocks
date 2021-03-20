532678ed345a5e95824d78db38e34eabff528b5e
Brandon Spada <brandonsgh123127@gmail.com>
2021-03-20 16:27:53 -0400

    implement simple filter for ignore percent and robinhood data

--- intro/_data_gather.py
+++ intro/_data_gather.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas
 from matplotlib import pyplot as plt
 import yfinance as yf
 import datetime
--- intro/news_scraper.py
+++ intro/news_scraper.py
@@ -1,15 +1,31 @@
 from intro._data_gather import Gather
+import re
+import pandas as pd
+import csv
+from numpy.typing.tests.data import pass
 
 class News_Scraper(Gather):
     def __init__(self):
         Gather.__init__(self)
-        self.filtered_results=()
+        self.filtered_results=pd.DataFrame(columns=['hash','text'])
     def _filter_terms(self,tweets):
+        _list = {}
+        pattern = re.compile(".*?(\\bRobinhood\\b|%|\\bmembers\\b).*")
         for tweet in tweets:
-            print(tweet.text)
+            if not pattern.match(tweet.text):
+                self.filtered_results=self.filtered_results.append({'hash':[hash(tweet.id)],'text':[tweet.text]},ignore_index=True)
+            else:
+                pass
+        print(self.filtered_results)
+    def load_tweet_csv(self):
+        pass
+    def save_tweet_csv(self,name):
+        pass
     ##start_date - mmddyyyy
     def get_news(self,term,start_date):
         self.filtered_results = self._filter_terms(self.get_recent_news(term,start_date,10))
+    def _append_data(self):
+        pass
 
 News_Scraper=News_Scraper()
-News_Scraper.get_news("AMD","01022021")
+News_Scraper.get_news("SPY","01022021")
