bc34bfac36a73ce71ac4a173aa9eeae08aedb7ba
Brandon Spada <brandonsgh123127@gmail.com>
2021-03-20 17:36:46 -0400

    implement working twitter API retriever

--- intro/_data_gather.py
+++ intro/_data_gather.py
@@ -5,9 +5,14 @@ import datetime
 from pandas_datareader import data as pdr
 import twitter
 
+'''Class Used for Gathering data through yfinance...
+'''
 class Gather():
+    def __repr__(self):
+        return 'stock_data.gather_data object <%s>' % ",".join(self.indicator)
     def __init__(self):
         yf.pdr_override()
+        # Local API Key for twitter account
         self.api = twitter.Api(consumer_key="wQ6ZquVju93IHqNNW0I4xn4ii",
                           consumer_secret="PorwKI2n1VpHznwyC38HV8a9xoDMWko4mOIDFfv2q7dQsFn2uY",
                           access_token_key="1104618795115651072-O3LSWBFVEPENGiTnXqf7cTtNgmNqUF",
@@ -19,6 +24,7 @@ class Gather():
         self.indicator = indicator
     def get_indicator(self):
         return self.indicator    
+    #For ease of data automation, we want to pass in randomized numbers without any special characters (mmddyyyy)
     def _convert_mdY_Ymd(self,date):
         return datetime.datetime.strptime(date,"%m%d%Y").strftime("%Y-%m-%d")
     # retrieve pandas_datareader object of data mmddyyyy to yyyymmdd
@@ -27,13 +33,11 @@ class Gather():
         return pdr.get_data_yahoo(self.indicator,start=formatted_dates[0],end=formatted_dates[1])
     # Twitter API Web Scraper for data on specific stocks
     def get_recent_news(self,term,date,amt=100):
-        print(f'Retrieving news using Twitter API... {term} {date}') 
+        print(f'Retrieving news using Twitter API... {term} {date} {amt}') 
         format_date=self._convert_mdY_Ymd(date)
         results = self.api.GetSearch(
-            raw_query=f'q=%24{term}%20min_faves%3A1%20',
-            result_type="recent",
-            since=format_date,
-            count=amt)
+            raw_query=f'q=%24{term}%20min_faves%3A1%20&result_type=popular%20&since={format_date}%20&count={amt}'
+            )
         return results
         
     
--- intro/news_scraper.py
+++ intro/news_scraper.py
@@ -1,30 +1,44 @@
 from intro._data_gather import Gather
 import re
 import pandas as pd
-import csv
+import csv,os,sys
 
+''' Class used for news data utilization from twitter api'''
 class News_Scraper(Gather):
+    def __repr__(self):
+        return 'stock_data.news_scraper object <%s>' % ",".join(self.indicator)
+
     def __init__(self):
         Gather.__init__(self)
         self.filtered_results=pd.DataFrame(columns=['hash','text'])
+    # Simple regex filter for disregard values
     def _filter_terms(self,tweets):
         _list = {}
         pattern = re.compile(".*?(\\bRobinhood\\b|%|\\bmembers\\b).*")
         for tweet in tweets:
-            if not pattern.match(tweet.text):
-                self.filtered_results=self.filtered_results.append({'hash':[hash(tweet.id)],'text':[tweet.text]},ignore_index=True)
+            if not pattern.match(tweet.text) and not self.filtered_results.hash.isin([hash(tweet.id)]).any().any():
+                self.filtered_results=self.filtered_results.append({'hash':hash(tweet.id),'text':tweet.text.replace('\n','')},ignore_index=True)
             else:
                 pass
-        print(self.filtered_results)
-    def load_tweet_csv(self):
-        pass
-    def save_tweet_csv(self,name):
-        pass
+    def load_tweet_csv(self,path):
+        try:
+            self.filtered_results = pd.read_csv(path)
+        except:
+            print("CSV empty, continuing...")
+        return 0
+    def save_tweet_csv(self,path):
+        self.filtered_results.to_csv(path,index=False,sep=',')
+        return 0
     ##start_date - mmddyyyy
     def get_news(self,term,start_date):
-        self.filtered_results = self._filter_terms(self.get_recent_news(term,start_date,10))
+        self.set_indicator(term)
+        self._filter_terms(self.get_recent_news(term,start_date,amt=10))
     def _append_data(self):
         pass
 
-News_Scraper=News_Scraper()
-News_Scraper.get_news("SPY","01022021")
+news=News_Scraper()
+news.load_tweet_csv(f':\\Users\\i-pod\\git\\{news.indicator.upper()}.csv')
+news.get_news("AMD","01022021")
+print(news.get_indicator())
+news.save_tweet_csv(f'C:\\Users\\i-pod\\git\\{news.indicator.upper()}.csv')
+print('done')
