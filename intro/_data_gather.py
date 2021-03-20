787cce16dde86508733e79dda612c369bc253e64
Brandon Spada <brandonsgh123127@gmail.com>
2021-03-20 14:12:33 -0400

    implement class form of basic data gather

--- intro/_data_gather.py
+++ intro/_data_gather.py
@@ -1,5 +1,3 @@
-import keras 
-import tensorflow as tf
 import numpy as np
 import pandas
 from matplotlib import pyplot as plt
@@ -7,11 +5,18 @@ import yfinance as yf
 import datetime
 from pandas_datareader import data as pdr
 
-# mmddyyyy to yyyymmdd
-# retrieve pandas_datareader object of data
-def get_data_from_range(indicator,start_date,end_date):
-    formatted_dates = (datetime.datetime.strptime(start_date,"%m%d%Y").strftime("%Y-%m-%d"),datetime.datetime.strptime(end_date,"%m%d%Y").strftime("%Y-%m-%d"))
-    return pdr.get_data_yahoo(indicator,start=formatted_dates[0],end=formatted_dates[1])
+
+class Gather():
+    def __init__(self):
+        yf.pdr_override()
+        self.indicator = ""
+        
+        
+    # mmddyyyy to yyyymmdd
+    # retrieve pandas_datareader object of data
+    def get_data_from_range(self,indicator,start_date,end_date):
+        formatted_dates = (datetime.datetime.strptime(start_date,"%m%d%Y").strftime("%Y-%m-%d"),datetime.datetime.strptime(end_date,"%m%d%Y").strftime("%Y-%m-%d"))
+        return pdr.get_data_yahoo(indicator,start=formatted_dates[0],end=formatted_dates[1])
     
-yf.pdr_override()
-print(get_data_from_range("AMD", "12212020", "03032021"))
+    def get_recent_news(self,indica):
+    
