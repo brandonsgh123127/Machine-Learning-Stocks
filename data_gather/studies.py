3b7c40a2fb36c77a345b07cd7d6183b45f132a5e
Brandon Spada <brandonsgh123127@gmail.com>
2021-03-21 23:37:26 -0400

    implement save data function

--- data_gather/studies.py
+++ data_gather/studies.py
@@ -2,6 +2,8 @@ from data_gather._data_gather import Gather
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+from pathlib import Path
 
 class Studies(Gather):
     def __repr__(self):
@@ -24,8 +26,19 @@ class Studies(Gather):
             self.applied_studies= self.applied_studies.append(({f'ema{length}':self.stock_data.ewm(span=int(length),adjust=False,halflife=half).mean()}),ignore_index=True)
         else:
             self.applied_studies= self.applied_studies.append(({f'ema{length}':self.stock_data.ewm(span=int(length),adjust=False).mean()}),ignore_index=True)
-
+        return 0
+    def save_data_csv(self,path):
+        concat_data = pd.concat([self.data],ignore_index=True)
+        concat_data.to_csv(path,index=False,sep=',')
+        return 0
 studies = Studies("SPY")
 Gather.set_data_from_range(studies,"01012020", "01012021")
 studies.get_data()
-studies.apply_ema("14")    
+studies.apply_ema("14")
+studies.apply_ema("30") 
+path = Path(os.getcwd()).parent.absolute()
+try:
+    os.mkdir("{0}/data/stock".format(path))
+except:
+    pass   
+studies.save_data_csv(f'{path}/data/stock/{Gather.get_indicator(studies)}.csv')
