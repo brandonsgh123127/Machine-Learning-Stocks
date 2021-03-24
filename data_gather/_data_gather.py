aacada5c5ddc05e3e6fb533b5d0206e01e02bc50
Brandon Spada <brandonsgh123127@gmail.com>
2021-03-23 23:01:52 -0400

    remove string date conversion, as not needed

--- data_gather/_data_gather.py
+++ data_gather/_data_gather.py
@@ -14,7 +14,7 @@ class Gather():
     MIN_RANGE = 5 # at least 5 days generated
     DAYS_IN_MONTH = {1:31,
                      2:28,
-                     2:31,
+                     3:31,
                      4:30,
                      5:31,
                      6:30,
@@ -42,33 +42,32 @@ class Gather():
         self.indicator = indicator
     def get_indicator(self):
         return self.indicator    
-    #For ease of data automation, we want to pass in randomized numbers without any special characters (mmddyyyy)
-    def _convert_mdY_Ymd(self,date):
-        return datetime.datetime.strptime(date,"%m%d%Y").strftime("%Y-%m-%d")
-    # retrieve pandas_datareader object of data mmddyyyy to yyyymmdd
+    # retrieve pandas_datareader object of datetime
     def set_data_from_range(self,start_date,end_date):
-        formatted_dates = (self._convert_mdY_Ymd(start_date),self._convert_mdY_Ymd(end_date))
-        self.data = pdr.get_data_yahoo(self.indicator,start=formatted_dates[0],end=formatted_dates[1])
+        self.data = pdr.get_data_yahoo(self.indicator,start=start_date.strftime("%Y-%m-%d"),end=end_date.strftime("%Y-%m-%d"))
         return self.data
+    def _reorder_dates(self,date1,date2):
+        if date1 < date2:
+            return (date1,date2) 
+        else:
+            return (date2,date1)
     # Generate random date for data generation
     def gen_random_dates(self):
         calc_leap_day = lambda year_month: random.randint(1,29) if year_month[1]==2 and ((year_month[0]%4==0 and year_month[0]%100==0 and year_month[0]%400==0) or (year_month[0]%4==0 and year_month[0]%100!=0)) else random.randint(1,28) if year_month[1]==2 else random.randint(1,self.DAYS_IN_MONTH[year_month[1]])
         set1 = (random.randint(self.MIN_DATE.year,self.MAX_DATE.year),random.randint(1,12))
-        set2 = (random.randint(self.MAX_DATE.year,self.MAX_DATE.year),random.randint(1,12))
+        set2 = (random.randint(self.MIN_DATE.year,self.MAX_DATE.year),random.randint(1,12))
         self.date_set = (datetime.datetime(set1[0],set1[1],calc_leap_day(set1)).date(),datetime.datetime(set2[0],set2[1],calc_leap_day(set2)).date())
         while abs(self.date_set[0].timetuple().tm_yday - self.date_set[1].timetuple().tm_yday) < self.MIN_RANGE: # abs value 
             print("Calculating new day, less than range")
             self.date_set[0].replace(day=calc_leap_day(set1))
+        self.date_set=self._reorder_dates(self.date_set[0],self.date_set[1])
         return self.date_set
     def get_data(self):
         return self.data
     # Twitter API Web Scraper for data on specific stocks
-    def get_recent_news(self,term,date,amt=100):
-        print(f'Retrieving news using Twitter API... {term} {date} {amt}') 
-        format_date=self._convert_mdY_Ymd(date)
+    def get_recent_news(self,term,start,end,amt=100):
+        print(f'Retrieving news using Twitter API...') 
         results = self.api.GetSearch(
-            raw_query=f'q=%24{term}%20min_faves%3A0%20&result_type=mixed%20&since={format_date}%20&count={amt}'
+            raw_query=f'q=%24{term}%20min_faves%3A0%20&result_type=mixed%20&since={start}%20&until={end}%20&count={amt}'
             )
         return results
-        
-    
