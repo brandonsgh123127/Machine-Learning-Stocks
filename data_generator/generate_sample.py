07c70d5e79b64adbe847ab7d07eebe0c179d586b
Brandon Spada <brandonsgh123127@gmail.com>
2021-04-18 12:08:07 -0400

    new display functions for working model charting

--- data_generator/generate_sample.py
+++ data_generator/generate_sample.py
@@ -8,6 +8,7 @@ This class will retrieve any file given, and take a sample of data, and retrieve
 class Sample(Normalizer):
     def __init__(self):
         self.normalizer = Normalizer()
+        self.normalizer.__init__()
         self.file_list = list()
         self.DAYS_SAMPLED = 15
         dirs = os.listdir(f'{self.normalizer.path}/data/stock_no_tweets')
@@ -15,24 +16,37 @@ class Sample(Normalizer):
             full_path = os.path.join(f'{self.normalizer.path}/data/stock_no_tweets',dir)
             for file in os.listdir(full_path):
                 self.file_list.append(f'{str(dir)}/{file}')
-    def generate_sample(self):
-        rand = random.choice(self.file_list)
+    def generate_sample(self,ticker=None):
+        if ticker is None:
+            rand = random.choice(self.file_list)
+        else:
+            rand = ticker
         self.normalizer.read_data(rand[rand.index('/')+1:rand.index('_')],rand[0:rand.index('/')]) # Get ticker and date from path
-        self.normalizer.convert_derivatives()
         # Iterate through dataframe and retrieve random sample
-        self.normalizer.normalized_data = [self.normalizer.normalized_data.iloc[x:x+self.DAYS_SAMPLED] for x in np.random.randint(len(self.normalizer.normalized_data), size=self.DAYS_SAMPLED)]
-        if len(self.normalizer.normalized_data[0]) < self.DAYS_SAMPLED:
+        self.normalizer.convert_derivatives()
+        # print(len(self.normalizer.normalized_data))
+
+        # x = np.random.randint(len(self.normalizer.normalized_data)/3,size=self.DAYS_SAMPLED)
+        # np.array_spli
+        self.normalizer.normalized_data = self.normalizer.normalized_data.iloc[:self.DAYS_SAMPLED]
+        # print(self.normalizer.normalized_data)
+        self.normalizer.normalize()
+        if len(self.normalizer.normalized_data) < self.DAYS_SAMPLED:
             self.normalizer.read_data(rand[rand.index('/')+1:rand.index('_')],rand[0:rand.index('/')]) # Get ticker and date from path
             self.normalizer.convert_derivatives()
             self.normalizer.normalized_data = self.normalizer.normalized_data.iloc[0:self.DAYS_SAMPLED]
+            self.normalizer.unnormalized_data = self.normalizer.normalized_data.iloc[0:self.DAYS_SAMPLED]
         else:
-            self.normalizer.normalized_data = self.normalizer.normalized_data[0]
+            self.normalizer.normalized_data = self.normalizer.normalized_data
+
         # self.normalizer.normalized_data = pd.DataFrame(self.normalizer.normalized_data,columns=[''])
             # try:
         # self.normalizer.normalized_data = self.normalizer.normalized_data.iloc[0:self.DAYS_SAMPLED+1]
             # except:
                 # self.normalizer.normalized_data = self.normalizer.normalized_data.iloc[0:self.DAYS_SAMPLED+1]
         return (rand[0:rand.index('/')],rand[rand.index('/')+1:rand.index('_')])
+    def unnormalize(self, data):
+        self.normalizer.unnormalize(data)
 # sampler = Sample()
 # for i in range(1,100000):
     # indicator = sampler.generate_sample()
--- data_generator/normalize_data.py
+++ data_generator/normalize_data.py
@@ -3,7 +3,9 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler,MinMaxScaler
+from sklearn.preprocessing import normalize
+
 '''
 Class that takes in studies and stock data, then transforms the data into a new dataframe.
 
@@ -15,15 +17,19 @@ class Normalizer():
         self.data= pd.DataFrame()
         self.studies= pd.DataFrame()
         self.normalized_data = pd.DataFrame()
+        self.unnormalized_data = pd.DataFrame()
         self.path = Path(os.getcwd()).parent.absolute() 
+        self.min_max = MinMaxScaler()
     def read_data(self,date,ticker):
         self.data = pd.read_csv(f'{self.path}/data/stock_no_tweets/{ticker}/{date}_data.csv').drop(['Adj Close','High','Low'],axis=1)
         self.studies = pd.read_csv(f'{self.path}/data/stock_no_tweets/{ticker}/{date}_studies.csv',index_col=False)
-        self.studies.reset_index()
         pd.set_option("display.max.columns", None)
     def convert_derivatives(self):
-        self.normalized_data = pd.DataFrame()
+        self.normalized_data = pd.DataFrame((),columns=['Open Diff','Close Diff','Derivative Diff','Derivative EMA14','Derivative EMA30','Close EMA14 Diff',
+                                                                                                'Close EMA30 Diff','EMA14 EMA30 Diff'])
 
+        self.normalized_data["Open Diff"] = self.data["Open"]
+        self.normalized_data["Close Diff"] = self.data["Close"]
         self.normalized_data["Derivative Diff"] = self.data["Open"]
         self.normalized_data["Derivative EMA14"] =self.studies["ema14"]
         self.normalized_data["Derivative EMA30"] =self.studies["ema30"]
@@ -33,6 +39,8 @@ class Normalizer():
 
         for index,row in self.data.iterrows():
             if index == 0:
+                self.normalized_data.loc[index,"Open Diff"] = 0
+                self.normalized_data.loc[index,"Close Diff"] = 0
                 self.normalized_data.loc[index,"Derivative Diff"] = 0
                 self.normalized_data.loc[index,"Derivative EMA14"] = 0
                 self.normalized_data.loc[index,"Derivative EMA30"] = 0        
@@ -41,25 +49,34 @@ class Normalizer():
                 self.normalized_data.loc[index,"EMA14 EMA30 Diff"] = self.studies.at[index,"ema14"] - self.studies.at[index,'ema30']
 
             else: # ((CLOSE - OPEN)2 -(CLOSE - OPEN)1) / (index2 -index1)
+                self.normalized_data.loc[index,"Close Diff"] = ((self.data.at[index,"Close"] - self.data.at[index-1,"Close"]))/(1)
+                self.normalized_data.loc[index,"Open Diff"] = ((self.data.at[index,"Open"] - self.data.at[index-1,"Open"]))/(1)
                 self.normalized_data.loc[index,"Derivative Diff"] = ((self.data.at[index,"Close"] - self.data.at[index,"Open"]) - (self.data.at[index-1,"Close"] - self.data.at[index-1,"Open"]))/(1)
                 self.normalized_data.loc[index,"Derivative EMA14"] = (self.studies.at[index,'ema14']-self.studies.at[index-1,'ema14'])/1
                 self.normalized_data.loc[index,"Derivative EMA30"] = (self.studies.at[index,'ema30']-self.studies.at[index-1,'ema30'])/1
                 self.normalized_data.loc[index,"Close EMA14 Diff"] = self.data.at[index,"Close"] - self.studies.at[index,'ema14']
                 self.normalized_data.loc[index,"Close EMA30 Diff"] = self.data.at[index,"Close"] - self.studies.at[index,'ema30']
                 self.normalized_data.loc[index,"EMA14 EMA30 Diff"] = self.studies.at[index,"ema14"] - self.studies.at[index,'ema30']
-        scaler = MinMaxScaler() 
-        self.normalized_data = pd.DataFrame(scaler.fit_transform(self.normalized_data),columns=['Derivative Diff','Derivative EMA14','Derivative EMA30','Close EMA14 Diff',
+        # scaler = StandardScaler() 
+        return 0
+    def normalize(self):
+        self.unnormalized_data = self.normalized_data
+        # print(self.normalized_data)
+        self.normalized_data = pd.DataFrame(self.min_max.fit_transform(self.normalized_data),columns=['Open Diff','Close Diff','Derivative Diff','Derivative EMA14','Derivative EMA30','Close EMA14 Diff',
                                                                                                 'Close EMA30 Diff','EMA14 EMA30 Diff']) #NORMALIZED DATA STORED IN NP ARRAY
         return 0
-   
+    def unnormalize(self,data):
+        # self.min_max = MinMaxScaler()
+        # self.convert_derivatives()
+        return pd.DataFrame(self.min_max.inverse_transform((data.to_numpy())),columns=['Open Diff','Close Diff','Derivative Diff','Derivative EMA14','Derivative EMA30','Close EMA14 Diff',
+                                                                                            'Close EMA30 Diff','EMA14 EMA30 Diff']) #NORMALIZED DATA STORED IN NP ARRAY
     def display_line(self):
         #self.normalized_data['index'] = range(1, len(self.normalized_data) + 1)
         self.normalized_data.plot()
         plt.show()
 # norm = Normalizer()
-# norm.read_data("2014-03-29--2014-05-02","MAA")
+# norm.read_data("2016-03-18--2016-07-23","CCL")
 # DAYS_SAMPLED=15
 # norm.convert_derivatives()
-# norm.normalized_data = norm.normalized_data.iloc[0:DAYS_SAMPLED]
 # print(norm.normalized_data)
 # norm.display_line()
