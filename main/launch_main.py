d31017a3dc9652a860ca5bea8f15ddab2fd921b3
Brandon Spada <brandonsgh123127@gmail.com>
2021-04-24 18:59:39 -0400

    multithreadinggit add maingit add main

--- main/launch_main.py
+++ main/launch_main.py
@@ -5,12 +5,16 @@ import tkinter as tk
 import os
 from pathlib import Path
 from tkinter import ttk
+import threading
+import concurrent.futures
+import queue
 
 class GUI():
     def __init__(self):
         self.path = Path(os.getcwd()).parent.absolute()
         self.window = tk.Tk()
         self.output_image = None
+        self.job_queue = queue.Queue()
     def get_current_price(self):
         if self.boolean1.get():
             self.open = tk.Label(self.content,text="Open:")
@@ -49,37 +53,42 @@ class GUI():
         return self.img
     
     def run(self):
-        title_frame = tk.LabelFrame(self.window,text="Welcome to Stock Predictor!",width=900,height=900)
-        title_frame.pack(fill='both',expand='yes')
-        title = tk.Label(title_frame,text="Machine learning based prediction model that utilizes regression prediction models.")
-        subtitle = tk.Label(title_frame,text="Model produces output that contains:\n\
-                                                 predicted Open differences from past day\n\
-                                                 predicted Close differences from past day\n\
-                                                 derivative difference between both days\n\
-                                                 and other useful EMA predictions by utilizing the 14 and 30 EMA.")
-        title.pack()
-        subtitle.pack()
-        
-        self.content = ttk.Frame(self.window)
-        self.content.pack()
-        self.output_image = tk.Canvas(self.window,width=1920,height=1080)
-        self.stock_label = tk.Label(self.content,text="Stock:")
-        self.stock_label.grid(column=2,row=0)
-        self.stock_input = tk.Entry(self.content)
-        self.stock_input.grid(column=3,row=0)
-        self.boolean1 = tk.BooleanVar()
-        self.boolean1.set(False)
-        self.boolean2 = tk.BooleanVar()
-        self.boolean2.set(True)
-        self.is_not_closed = ttk.Checkbutton(self.content, text="Predict ongoing day?", variable=self.boolean1,command=self.get_current_price)
-        self.is_not_closed.grid(column=2, row=1)
-        self.has_actuals = ttk.Checkbutton(self.content, text="Compare Mode?", variable=self.boolean2)
-        self.has_actuals.grid(column=4, row=1)
-        self.output_image.pack(expand='yes', fill='both')
-        self.generate_button = ttk.Button(self.content, text="Generate",command= lambda: self.load_model(self.stock_input.get(),self.boolean2.get(),self.boolean1.get()))
-        self.generate_button.grid(column=3, row=2)
-        self.window.mainloop()
-
+        with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+            threads = []
+            title_frame = tk.LabelFrame(self.window,text="Welcome to Stock Predictor!",width=900,height=900)
+            title_frame.pack(fill='both',expand='yes')
+            title = tk.Label(title_frame,text="Machine learning based prediction model that utilizes regression prediction models.")
+            subtitle = tk.Label(title_frame,text="Model produces output that contains:\n\
+                                                     predicted Open differences from past day\n\
+                                                     predicted Close differences from past day\n\
+                                                     derivative difference between both days\n\
+                                                     and other useful EMA predictions by utilizing the 14 and 30 EMA.")
+            title.pack()
+            subtitle.pack()
+            
+            self.content = ttk.Frame(self.window)
+            self.content.pack()
+            self.output_image = tk.Canvas(self.window,width=1920,height=1080)
+            self.stock_label = tk.Label(self.content,text="Stock:")
+            self.stock_label.grid(column=2,row=0)
+            self.stock_input = tk.Entry(self.content)
+            self.stock_input.grid(column=3,row=0)
+            self.boolean1 = tk.BooleanVar()
+            self.boolean1.set(False)
+            self.boolean2 = tk.BooleanVar()
+            self.boolean2.set(True)
+            self.is_not_closed = ttk.Checkbutton(self.content, text="Predict ongoing day?", variable=self.boolean1,command=executor.submit(self.get_current_price))
+            self.is_not_closed.grid(column=2, row=1)
+            self.has_actuals = ttk.Checkbutton(self.content, text="Compare Mode?", variable=self.boolean2)
+            self.has_actuals.grid(column=4, row=1)
+            self.output_image.pack(expand='yes', fill='both')
+            self.generate_button = ttk.Button(self.content, text="Generate",command= lambda: self.job_queue.put(executor.submit(self.load_model,self.stock_input.get(),self.boolean2.get(),self.boolean1.get())))
+            self.generate_button.grid(column=3, row=2)
+            self.window.mainloop()
+            while True:
+                if not self.job_queue.empty():
+                    item = self.job_queue.get(0)
+                    
 
 if __name__ == '__main__':
     ui = GUI()
