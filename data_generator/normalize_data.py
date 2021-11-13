fix functionality that comes with new normalization values for out=10

data_generator/normalize_data.py
@@ -102,7 +102,10 @@ class Normalizer():
             self.keltner=vals[3]
         except Exception as e:
             print(f'[ERROR] Failed to retrieve data points for {ticker} from {initial_date.strftime("%Y-%m-%d")} to {(initial_date - datetime.timedelta(days=40)).strftime("%Y-%m-%d")}!\nException:\n',str(e))
-            raise Exception
+            raise AssertionError
+        except:
+            print(f'[ERROR] Failed to retrieve data points for {ticker} from {initial_date.strftime("%Y-%m-%d")} to {(initial_date - datetime.timedelta(days=40)).strftime("%Y-%m-%d")}!\nException:\n',str(e))
+            raise AssertionError
         self.cnx.close()
         return 
     def reset_data(self):
@@ -151,26 +154,19 @@ class Normalizer():
     def convert_derivatives(self,out=8):
         data = self.data
         try:
-            data = self.data.drop(columns={'Date'})
+            data = data.drop(columns={'Date'})
         except:
             pass
         data = data.astype('float')
         self.studies = self.studies.astype('float')
+        self.keltner = self.keltner.astype('float')
         self.normalized_data = pd.DataFrame((),columns=['Open EMA Euclidean','Close EMA Euclidean',
                                                         'Open EMA14 Euclidean','Open EMA30 Euclidean',
                                                         'Close EMA14 Euclidean','Close EMA30 Euclidean',
                                                         'EMA14 EMA30 Euclidean', 'Prior Close Euclidean',
                                                         'Upper Keltner Close Diff', 'Lower Keltner Close Diff'])
-        self.normalized_data["Open EMA Euclidean"] = data["Open"]
-        self.normalized_data["Close EMA Euclidean"] =data["Close"]
-        self.normalized_data["Open EMA14 Diff"] = self.studies["ema14"]
-        self.normalized_data["Open EMA30 Diff"] = self.studies["ema30"]
-        self.normalized_data["Close EMA14 Diff"] = self.studies["ema14"]
-        self.normalized_data["Close EMA30 Diff"] = self.studies["ema30"]
-        self.normalized_data["EMA14 EMA30 Diff"] = self.studies["ema14"]
-        self.normalized_data["EMA14 EMA30 Diff"] = data["Close"]
-        self.normalized_data["Upper Keltner Close Diff"] = data["Close"]
-        self.normalized_data["Lower Keltner Close Diff"] = data["Close"]
+
+
         
 
         for index,row in data.iterrows():
@@ -183,21 +179,21 @@ class Normalizer():
                     self.normalized_data.loc[index,"Close EMA14 Euclidean"] = np.power(np.power((((data.at[index,"Close"] - self.studies.at[index,'ema14']))),2),1/2)
                     self.normalized_data.loc[index,"Close EMA30 Euclidean"] = np.power(np.power((((data.at[index,"Close"] - self.studies.at[index,'ema30']))),2),1/2)
                     self.normalized_data.loc[index,"EMA14 EMA30 Euclidean"] = np.power(np.power((((self.studies.at[index,"ema14"] - self.studies.at[index,'ema30']))),2),1/2)
-                    self.normalized_data.loc[index,"Prior Close Euclidean"] = 0
-                    self.normalized_data.loc[index,"Upper Keltner Close Diff"] = data.at[index,"upper"] - self.studies.at[index,'Close']
-                    self.normalized_data.loc[index,"Lower Keltner Close Diff"] = data.at[index,"lower"] - self.studies.at[index,'Close']
+                    self.normalized_data.loc[index,"Prior Close Euclidean"] = (0)
+                    self.normalized_data.loc[index,"Upper Keltner Close Diff"] = (self.keltner.at[index,"upper"] - data.at[index,'Close'])
+                    self.normalized_data.loc[index,"Lower Keltner Close Diff"] = (self.keltner.at[index,"lower"] - data.at[index,'Close'])
     
                 else:
                     self.normalized_data.loc[index,"Open EMA Euclidean"] = np.power(np.power(((data.at[index,"Open"] - self.studies.at[index,"ema14"])+(data.at[index,"Open"] - self.studies.at[index,"ema30"])),2),1/2)
                     self.normalized_data.loc[index,"Close EMA Euclidean"] = np.power(np.power(((data.at[index,"Close"] - self.studies.at[index,"ema14"])+(data.at[index,"Close"] - self.studies.at[index,"ema30"])),2),1/2)       
-                    self.normalized_data.loc[index,"Open EMA14 Euclidean"] = (data.at[index,"Open"] - self.studies.at[index,'ema14'])/data.at[index,"Open"]
-                    self.normalized_data.loc[index,"Open EMA30 Euclidean"] = (data.at[index,"Open"] - self.studies.at[index,'ema30'])/data.at[index,"Open"]
-                    self.normalized_data.loc[index,"Close EMA14 Euclidean"] = (data.at[index,"Close"] - self.studies.at[index,'ema14'])/data.at[index,"Close"]
-                    self.normalized_data.loc[index,"Close EMA30 Euclidean"] = (data.at[index,"Close"] - self.studies.at[index,'ema30'])/data.at[index,"Close"]
+                    self.normalized_data.loc[index,"Open EMA14 Euclidean"] = np.power(np.power((((data.at[index,"Open"] - self.studies.at[index,'ema14']))),2),1/2)
+                    self.normalized_data.loc[index,"Open EMA30 Euclidean"] = np.power(np.power((((data.at[index,"Open"] - self.studies.at[index,'ema30']))),2),1/2)
+                    self.normalized_data.loc[index,"Close EMA14 Euclidean"] = np.power(np.power((((data.at[index,"Close"] - self.studies.at[index,'ema14']))),2),1/2)
+                    self.normalized_data.loc[index,"Close EMA30 Euclidean"] = np.power(np.power((((data.at[index,"Close"] - self.studies.at[index,'ema30']))),2),1/2)
                     self.normalized_data.loc[index,"EMA14 EMA30 Euclidean"] = np.power(np.power((((self.studies.at[index,"ema14"] - self.studies.at[index,'ema30']))),2),1/2)
-                    self.normalized_data.loc[index,"Prior Close Euclidean"] = np.power(np.power((((data.at[index,"Close"] - self.studies.at[index-1,'Close']))),2),1/2)
-                    self.normalized_data.loc[index,"Upper Keltner Close Diff"] = data.at[index,"upper"] - self.studies.at[index,'Close']
-                    self.normalized_data.loc[index,"Lower Keltner Close Diff"] = data.at[index,"lower"] - self.studies.at[index,'Close']
+                    self.normalized_data.loc[index,"Prior Close Euclidean"] = np.power(np.power((((data.at[index,"Close"] - data.at[index-1,'Close']))),2),1/2)
+                    self.normalized_data.loc[index,"Upper Keltner Close Diff"] = (self.keltner.at[index,"upper"] - data.at[index,'Close'])
+                    self.normalized_data.loc[index,"Lower Keltner Close Diff"] = (self.keltner.at[index,"lower"] - data.at[index,'Close'])
             except:
                 pass
         # print(self.normalized_data,len(self.studies),len(data))

machine_learning/neural_network.py
@@ -119,13 +119,14 @@ class Network(Neural_Framework):
             train_targets=[]
             models[i] = 1
             for j in range(1,self.BATCHES):
-                train= []
-                train_targets=[]
                 try:
                     self.generate_sample(True, rand_date)
                 except:
+                    j=j-1
                     continue
                 try:
+                    # print(train)
+                    # print(len(self.sampler.normalized_data.iloc[:-1].to_numpy()))
                     train.append(np.reshape(self.sampler.normalized_data.iloc[:-1].to_numpy(),(1,1,140)))# Retrieve all except for last data to be analyzed/predicted
                     if self.model_choice <= 3:
                         train_targets.append(np.reshape(self.sampler.normalized_data.iloc[-1:].to_numpy(),(1,10)))
@@ -134,19 +135,13 @@ class Network(Neural_Framework):
                         tmp = pd.concat([pd.DataFrame([tmp['Open'].to_numpy()]),pd.DataFrame([tmp['Close'].to_numpy()]),pd.DataFrame([tmp['Range'].to_numpy()])])
                         train_targets.append(np.reshape(tmp.to_numpy(),(1,3)))
                 except Exception as e:
-                    print('[ERROR] Failed to specify train_target value!\nException:\n',str(e))
+                    print('[ERROR] Failed to specify train_target value!\nException:\n',str(e),flush=True)
                     continue
-            for j in range(1,len(train)):
-                disp = self.nn.train_on_batch(np.stack(train[j]), np.stack(train_targets[j]))
-                model_info = {'model' : self.nn, 'history' : disp[1], 'loss' : disp[0]}
-                models[i] = (model_info['loss'] + models[i] )
-                try:
-                    self.nn.evaluate(np.stack(train[j]),np.stack(train_targets[j]),verbose=1)
-                    # print(f'loss : {loss}\t accuracy : {acc}')
                 except:
-                    print('[ERROR] Could not evaluate model')
+                    print('[ERROR] Unknown error has occurred while training!')
                     continue
-            models[i] = models[i] / self.BATCHES
+            # Use fit for generating with ease.  Validation data included for analysis of loss
+            disp = self.nn.fit(x=np.stack(train), y=np.stack(train_targets),batch_size=1,epochs=1,validation_split=0.177)
             self.save_model()
 
         return models
@@ -439,9 +434,11 @@ def run(epochs,batch_size,name="model_relu"):
     neural_net.save_model()
 
 
-run(100,100,"model_relu")
-# run(100,100,"model_leaky")
-# run(100,100,"model_sigmoid")
-# run(50,100,"model_relu2")
-# run(100,100,"model_leaky2")
-# run(100,100,"model_sigmoid2")
+# run(97,200,"model_relu")
+run(88,200,"model_leaky")
+run(100,200,"model_sigmoid")
+run(50,200,"model_relu2")
+run(100,200,"model_leaky2")
+run(100,200,"model_sigmoid2")
+# net=Network(1,1)
+load("SPY",False,"model_relu",True,device_opt='/device:GPU:0',rand_date=False,data=None)     